344612180717f1d56f1ef8dc566642331be61521
DavilsonJunior <davilsonjr_jobs@yahoo.com>
2022-04-15 19:39:21 -0300

    feat: added balance and valid type of transaction

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -9,10 +9,10 @@ const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    const transaction = transactionsRepository.all();
-    // const balance = transactionsRepository.getBalance();
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
 
-    return response.json(transaction);
+    return response.json({ transactions, balance });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
